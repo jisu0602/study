f7e42d79f6b590f331c14e73ecd1718ae0f54763
jisu0602 <56910659+jisu0602@users.noreply.github.com>
2020-03-21 17:21:59 +0900

    Update ch3.py

--- python/ch3.py
+++ python/ch3.py
@@ -1,1 +1,31 @@
 #ch3
+'''
+비교 연산자: ==, !=, <, >
+논리 연산자: not, and, or
+'''
+x = 10
+under_20 = x < 20
+print("under_20:", under_20)
+print("not under_20:", not under_20)
+print()
+
+'''
+if 조건문
+조건 분기: 조건을 기반으로 실행의 흐름을 변경하는 것
+'''
+
+입력 받기
+number = input("정수 입력> ")
+number = int(number)
+
+양수 조건
+if number > 0
+print("양수!")
+
+음수 조건
+if number < 0
+print("음수!")
+
+0 조건
+if number = 0
+print("0임")
