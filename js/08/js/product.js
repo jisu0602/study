261950a99b720ad1c5a361ee3e8336f14c67d572
jisu0602 <56910659+jisu0602@users.noreply.github.com>
2020-10-28 22:30:50 +0900

    Add files via upload

--- js/08/js/product.js
+++ js/08/js/product.js
@@ -1,5 +1,8 @@
 		var bigPic = document.querySelector("#cup");  
 		var smallPics = document.querySelectorAll(".small");  
+		var view = document.querySelector("#view");
+		var isOpen = false;
+
 
 		for(i=0; i<smallPics.length; i++) {
 			smallPics[i].addEventListener("click", function() {
@@ -8,8 +11,18 @@
 			});
 		}
 
-		var view = document.querySelector("#view");
-
 		view.addEventListener("click", function() {
-			document.querySelector("#detail").style.display = "block";
+			//document.querySelector("#detail").style.display = "block";
+			if(isOpen == false) {
+				document.querySelector("#detail").style.display = "block";
+				view.innerText = "상세 설명 닫기";
+				isOpen = true;
+			}
+			else {
+				document.querySelector("#detail").style.display = "none";
+				view.innerText = "상세 설명 보기";
+				isOpen = false;
+			}
 		});
+
+		
